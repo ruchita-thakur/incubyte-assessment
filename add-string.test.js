39305021f4dac56c0e8c3e4639023c4476689378
rohit-thakur12 <thakurruchita24@gmail.com>
2024-09-27 20:06:40 +0530

    added second test case

--- add-string.test.js
+++ add-string.test.js
@@ -4,4 +4,7 @@ describe("add", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+  it("should return the number itself for a single number string", () => {
+    expect(add("5").toBe(5));
+  });
 });
